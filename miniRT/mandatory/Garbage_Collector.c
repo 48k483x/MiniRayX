dadf52d570c16a678694d368ac75ba95794ff6d6
Void.ID <romaissaeabde@gmail.com>
2024-07-01 22:43:20 +0100

    HAYDT DOUK LFILES LIMA3NDNA MANDIROU BIHOUM, DABA miniRT HOUWA KOULSHI

---
+++ miniRT/mandatory/Garbage_Collector.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Garbage_Collector.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aouhadou <aouhadou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/29 21:13:09 by smia              #+#    #+#             */
+/*   Updated: 2022/09/25 14:05:13 by aouhadou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minirt.h"
+
+t_collector	*new_node(void *adr)
+{
+	t_collector	*node;
+
+	node = malloc(sizeof(t_collector));
+	if (!node)
+		return (NULL);
+	node->adr = adr;
+	node->next = NULL;
+	return (node);
+}
+
+t_collector	**append_adr(t_collector **g_root, void *adr)
+{
+	t_collector	*node;
+	t_collector	*tmp;
+
+	node = new_node(adr);
+	if (!*g_root)
+		return (*g_root = node, g_root);
+	tmp = *g_root;
+	while (tmp->next)
+		tmp = tmp->next;
+	tmp->next = node;
+	return (g_root);
+}
+
+void	*ft_malloc(t_collector **g_root, size_t size)
+{
+	void	*ptr;
+
+	ptr = malloc(size);
+	if (!ptr)
+		return (NULL);
+	else
+		return (append_adr(g_root, ptr), ptr);
+}
+
+void	ft_collect(t_collector **g_root, t_collector *node)
+{
+	if (!node)
+		return ;
+	ft_collect(g_root, node->next);
+	free(node->adr);
+	free(node);
+}
---
+++ miniRT/mandatory/libft_2.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   split_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: void_id <void_id@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/23 13:41:47 by aouhadou          #+#    #+#             */
+/*   Updated: 2024/07/01 22:36:48 by void_id          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minirt.h"
+
+int	is_digit(char c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
+
+void	*_malloc(size_t size)
+{
+	void	*ptr;
+
+	ptr = malloc(size);
+	if (!ptr)
+		return (NULL);
+	return (ptr);
+}
+
+int	is_digitf(char *s)
+{
+	int i;
+	int dot;
+
+	dot = 0;
+	i = 0;
+	if (s == NULL || *s == '\0')
+		return (0);
+	if (s[i] == '-')
+		i++;
+	if (s[i] == '\0' || s[i] == '.')
+		return (0);
+	while (s[i])
+	{
+		if (s[0] == '.' || s[_strlen(s) - 1] == '.')
+			return (0);
+		if (s[i] == '.')
+			dot++;
+		if ((!is_digit(s[i]) && s[i] != '.') || dot > 1)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+
+int	is_space(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] == ' ' || s[i] == '\t')
+		i++;
+	if (s[i] == '\0')
+		return (1);
+	return (0);
+}
+
+void	*_memdel(void *ptr)
+{
+	if (ptr)
+	{
+		free(ptr);
+		ptr = NULL;
+	}
+	return (NULL);
+}
+
